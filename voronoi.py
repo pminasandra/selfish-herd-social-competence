03b9f2e3100a4489a75121ac8325283609941a99
Cecilia Baldoni <cbaldoni@ab.mpg.de>
2024-12-10 11:46:06 +0100

    mirrors

--- voronoi.py
+++ voronoi.py
@@ -6,3 +6,24 @@
 Provides methods to construct Voronoi polygons for groups of simulated animals
 and compute their areas.
 """
+
+def mirror_bottom(y_old):
+    y_new = -y_old
+    return y_new
+
+def mirror_top(y_old):
+    y_new = 2-y_old
+    return y_new
+
+def mirror_left(x_old):
+    x_new = -x_old
+    return x_new
+
+def mirror_right(x_old):
+    x_new = 2-x_old
+    return x_new
+
+print(mirror_bottom(0.2))  
+print(mirror_top(0.8))
+print(mirror_left(0.3))
+print(mirror_right(0.7))
