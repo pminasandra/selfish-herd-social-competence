1974a8b3cf2a3f39a8dddc67038117c67cc3801c
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-12-10 12:05:43 +0100

    Clean little mirroring function

--- voronoi.py
+++ voronoi.py
@@ -32,21 +32,25 @@ def mirror_right(locations):
     new_locs[:, 0] += 2.0
     return new_locs
 
+def mirror_unit_sq(locations):
+    mirrored_locs = []
+    mirrored_locs.append(mirror_left(locations))
+    mirrored_locs.append(mirror_right(locations))
+    mirrored_locs.append(mirror_top(locations))
+    mirrored_locs.append(mirror_bottom(locations))
+
+    mirrored_locs = np.vstack(mirrored_locs)
+    return mirrored_locs
+
 if __name__ == "__main__":
     locs = np.random.uniform(size=(20, 2))
-    bottoms = mirror_bottom(locs)
-    tops = mirror_top(locs)
-    lefts = mirror_left(locs)
-    rights = mirror_right(locs)
+    mirrored_locs = mirror_unit_sq(locs)
 
     plt.scatter(locs[:, 0], locs[:, 1], color="black", s=0.3)
-    plt.scatter(bottoms[:, 0], bottoms[:, 1], color="gray", s=0.3)
-    plt.scatter(tops[:, 0], tops[:, 1], color="gray", s=0.3)
-    plt.scatter(lefts[:, 0], lefts[:, 1], color="gray", s=0.3)
-    plt.scatter(rights[:, 0], rights[:, 1], color="gray", s=0.3)
+    plt.scatter(mirrored_locs[:, 0], mirrored_locs[:, 1], color="gray", s=0.3)
 
-    plt.axvline(0, linestyle="dotted")
-    plt.axvline(1, linestyle="dotted")
-    plt.axhline(0, linestyle="dotted")
-    plt.axhline(1, linestyle="dotted")
+    plt.axvline(0, linestyle="dotted", linewidth=0.3)
+    plt.axvline(1, linestyle="dotted", linewidth=0.3)
+    plt.axhline(0, linestyle="dotted", linewidth=0.3)
+    plt.axhline(1, linestyle="dotted", linewidth=0.3)
     plt.show()
