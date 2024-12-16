c3b4be2b2032095706118f393658fa4561770bc3
Pranav Minasandra [CoCoMo cluster] <severuscool@gmail.com>
2024-12-16 15:32:37 +0100

    One more fix

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ if __name__ == "__main__":
     if config.RUN_SIMS:
         depth_dirs = []
         for pop_size in config.POP_SIZES:
-            depth_dirs.append([joinpath(config.DATA, str(pop_size), f"d{depth}")\
+            depth_dirs.extend([joinpath(config.DATA, str(pop_size), f"d{depth}")\
                             for depth in config.DEPTHS_OF_REASONING])
         [os.makedirs(dir_, exist_ok=True) for dir_ in depth_dirs]
 
         for p_size in config.POP_SIZES:
             pop_size = str(p_size)
             print("Working on pop_size", pop_size)
-            inits = [np.random.uniform(size=(pop_size, 2))\
+            inits = [np.random.uniform(size=(p_size, 2))\
                         for i in range(config.NUM_REPEATS)]
             init_names = [str(uuid.uuid4()) for i in range(config.NUM_REPEATS)]
 
             for depth in config.DEPTHS_OF_REASONING:
                 print("Depth of reasoning:", depth)
-                herds = [selfishherd.SelfishHerd(pop_size, depth, loc) for loc\
+                herds = [selfishherd.SelfishHerd(p_size, depth, loc) for loc\
                             in inits]
                 filenames = [joinpath(config.DATA, pop_size, f"d{depth}",
                                     f"{pop_size}-{depth}-uname.pkl")\
