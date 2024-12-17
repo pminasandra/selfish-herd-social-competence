ace018af2df5360011fef3d757647366c89ac4c2
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-12-17 13:37:07 +0100

    Fixed final little bugs

--- main.py
+++ main.py
@@ -26,19 +26,18 @@ if __name__ == "__main__":
                             for depth in config.DEPTHS_OF_REASONING])
         [os.makedirs(dir_, exist_ok=True) for dir_ in depth_dirs]
 
-        for p_size in config.POP_SIZES:
-            pop_size = str(p_size)
+        for pop_size in config.POP_SIZES:
             print("Working on pop_size", pop_size)
-            inits = [np.random.uniform(size=(p_size, 2))\
+            inits = [np.random.uniform(size=(pop_size, 2))\
                         for i in range(config.NUM_REPEATS)]
             init_names = [str(uuid.uuid4()) for i in range(config.NUM_REPEATS)]
 
             for depth in config.DEPTHS_OF_REASONING:
                 print("Depth of reasoning:", depth)
-                herds = [selfishherd.SelfishHerd(p_size, depth, loc) for loc\
+                herds = [selfishherd.SelfishHerd(pop_size, depth, loc) for loc\
                             in inits]
-                filenames = [joinpath(config.DATA, pop_size, f"d{depth}",
-                                    f"{pop_size}-{depth}-uname.pkl")\
+                filenames = [joinpath(config.DATA, str(pop_size), f"d{depth}",
+                                    f"{pop_size}-{depth}-{uname}.pkl")\
                                     for uname in init_names]
                 args = zip(herds, filenames)
                 
