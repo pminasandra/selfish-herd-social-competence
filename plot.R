c5fc819f8450cf912d31915dc362364e7860396b
Cecilia Baldoni <cbaldoni@ab.mpg.de>
2024-12-19 17:06:03 +0100

    Modified ggplot

--- plot.R
+++ plot.R
@@ -2,6 +2,7 @@
 ### Libraries
 library(tidyverse)
 
+## Data handling
 
 csv_dir <- "~/Downloads/results-typical-group-size"
 file_list <- list.files(path = csv_dir, pattern = "*.csv", full.names = TRUE)
@@ -12,7 +13,6 @@ data_list <- lapply(file_list, function(file) {
     message("Empty file: ", file, ", skipping")
     return(NULL)
   }
-
   data$type <- gsub("\\.csv$", "", basename(file))
   split_values <- strsplit(data$type, "-")
   data$pop_size <- sapply(split_values, `[`, 1)
@@ -31,13 +31,14 @@ combined_data <- combined_data %>%
   relocate(reasoning, .after = pop_size)
 write.csv(combined_data, "combined_data.csv", row.names = FALSE)
 
-## GGplots
-
+# Convert into long format
 long_data <- combined_data %>%
   pivot_longer(cols = starts_with("t"),
                names_to = "time",
                values_to = "group_size") %>%
   mutate(time = as.numeric(gsub("t", "", time)))
+
+## Plotting
 
 ggplot(long_data, aes(x = time, y = group_size, color = pop_size, linetype = reasoning)) +
   geom_line(aes(group = interaction(uname, pop_size, reasoning)), alpha = 0.7) +
@@ -50,9 +51,19 @@ ggplot(long_data, aes(x = time, y = group_size, color = reasoning)) +
   geom_point(size = 0.5, alpha = 0.8) +
   facet_wrap(~reasoning)
 
-ggplot(long_data, aes(x = time, y = group_size, color = reasoning)) +
+plot <- ggplot(long_data, aes(x = time, y = group_size, color = reasoning)) +
   #geom_line(aes(group = interaction(uname, pop_size, reasoning)), alpha = 0.1) +
   geom_smooth(aes(group = interaction(pop_size, reasoning)), method = "loess", fill = 'orange') +
   facet_wrap(~pop_size, scales = "free") +
-  theme_bw()
+  theme_bw() +
+  theme(axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12))
+
+## Save as pdf in right folder
+cw_file <- "~/code/.cw"
+directory_path <- readLines(cw_file, warn = FALSE)
+
+ggsave(file.path(file.path(directory_path, "Figures"), "group_size_plot.pdf"), plot = plot, width = 8, height = 6, units = "in")
 
