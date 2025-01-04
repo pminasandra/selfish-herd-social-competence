2f8048d17c8a01e1c3f5f82d2fe18df991e474b4
Cecilia Baldoni <cbaldoni@ab.mpg.de>
2025-01-04 14:59:57 +0100

    Modified plots

--- plot.R
+++ plot.R
@@ -42,7 +42,9 @@ group_longdata <- group_data %>%
   pivot_longer(cols = starts_with("t"),
                names_to = "time",
                values_to = "group_size") %>%
-  mutate(time = as.numeric(gsub("t", "", time)))
+  mutate(time = as.numeric(gsub("t", "", time)),
+         pop_size = factor(pop_size, levels = unique(as.character(sort(as.numeric(pop_size))))),
+         reasoning = as.factor(reasoning))
 
 ### EDA ####
 
@@ -69,37 +71,36 @@ ggplot(group_longdata, aes(x = time, y = group_size, color = pop_size)) +
         axis.text.y = element_text(size = 12)) +
   facet_wrap(~reasoning) 
 
-plot <- ggplot(group_longdata, aes(x = time, y = group_size, color = reasoning)) +
-  #geom_line(aes(group = interaction(uname, pop_size, reasoning)), alpha = 0.1) +
+plot_groupsize <- ggplot(group_longdata, aes(x = time, y = group_size, color = reasoning)) +
   geom_smooth(aes(group = interaction(pop_size, reasoning)), method = "loess", fill = 'orange') +
   facet_wrap(~pop_size, scales = "free") +
+  labs(y= "Group Size", x = "Time (s)") +
   theme_bw() +
   theme(axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
         axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12))
 
-ggsave(file.path(file.path(directory_path, "Figures"), "group_size_plot.pdf"), plot = plot, width = 8, height = 6, units = "cm")
+ggsave(file.path(file.path(directory_path, "Figures"), "group_size_plot.pdf"), plot = plot_groupsize, width = 8, height = 6, units = "cm")
 
 ### Data analysis ####
 average_group_size <- group_longdata %>%
   group_by(time, pop_size, reasoning) %>%
+  mutate(reasoning = factor(reasoning, levels = c("d0", "d1", "d2", "d3"))) %>% 
   summarize(mean_group_size = mean(group_size, na.rm = TRUE),
             median_group_size = median(group_size, na.rm = TRUE),
             sd_group_size = sd(group_size, na.rm = TRUE),
             .groups = "drop")
 ggplot(average_group_size, aes(x = time, y = mean_group_size, color = pop_size)) +
-  geom_line(size = 1) +
+  geom_line(linewidth = 1) +
   facet_wrap(~reasoning) +
+  labs(y= "Mean Group Size", x = "Time (s)", color = "Population Size") +
   theme_bw()
 
-average_group_size %>%
-  group_by(pop_size, reasoning) %>%
-  filter(mean_group_size == max(mean_group_size)) %>%
-  arrange(pop_size, reasoning)
-
-average_group_size <- average_group_size %>%
-  mutate(reasoning = factor(reasoning, levels = c("d0", "d1", "d2", "d3")))
+# average_group_size %>%
+#   group_by(pop_size, reasoning) %>%
+#   filter(mean_group_size == max(mean_group_size)) %>%
+#   arrange(pop_size, reasoning)
 
 # Run a two-way ANOVA, pairwise comparison and plot emmeans
 anova_result <- aov(mean_group_size ~ reasoning * pop_size, data = average_group_size)
@@ -108,11 +109,13 @@ summary(anova_result)
 emmeans(anova_result, ~ reasoning | pop_size)
 pairwise_df <- as.data.frame(emmeans(anova_result, ~ reasoning | pop_size))
 
-emmeans <- ggplot(pairwise_df, aes(x = reasoning, y = emmean, color = pop_size, group = pop_size)) +
-  geom_line(size = 1) + geom_point(size = 2) +
+emmeans_plot <- ggplot(pairwise_df, aes(x = reasoning, y = emmean, color = pop_size, group = pop_size)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 2) +
+  geom_ribbon(aes(ymin = lower.CL, ymax = upper.CL, fill = pop_size), alpha = 0.2, color = NA) + # Shaded CI
+  labs(y= "EMMean Group Size", x = "Depth of Reasoning", color = "Population Size", fill = "Population Size") +
   theme_bw()
-
-ggsave(file.path(file.path(directory_path, "Figures"), "emmeans.pdf"), plot = emmeans, width = 8, height = 6, units = "cm")
+ggsave(file.path(file.path(directory_path, "Figures"), "emmeans.pdf"), plot = emmeans_plot, width = 8, height = 6, units = "cm")
 
 
 ## Load area file from Data/median-area folder ####
@@ -145,7 +148,9 @@ area_longdata <- area_data %>%
   pivot_longer(cols = starts_with("t"),
                names_to = "time",
                values_to = "area_size") %>%
-  mutate(time = as.numeric(gsub("t", "", time)))
+  mutate(time = as.numeric(gsub("t", "", time)),
+         pop_size = factor(pop_size, levels = unique(as.character(sort(as.numeric(pop_size))))),
+         reasoning = as.factor(reasoning))
 
 ### EDA ####
 ggplot(area_longdata, aes(x = time, y = area_size, color = pop_size)) +
@@ -163,20 +168,42 @@ ggplot(area_longdata, aes(x = time, y = area_size, color = pop_size)) +
         axis.text.y = element_text(size = 12)) +
   facet_wrap(~pop_size, scales = "free")
 
-ggplot(area_longdata, aes(x = time, y = area_size, color = reasoning)) +
-  geom_line(aes(group = interaction(uname, pop_size, reasoning)), alpha = 0.1) +
-  theme(axis.title.x = element_text(size = 14),
-        axis.title.y = element_text(size = 14),
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12)) +
-  facet_wrap(~reasoning, scales = "free") 
-
-plot <- ggplot(area_longdata, aes(x = time, y = area_size, color = reasoning)) +
-  geom_line(aes(group = interaction(uname, pop_size, reasoning)), colour = "lightblue", alpha = 0.05) +
+plot_area <- ggplot(area_longdata, aes(x = time, y = area_size, color = reasoning)) +
+  #geom_line(aes(group = interaction(uname, pop_size, reasoning)), alpha = 0.1) +
   geom_smooth(aes(group = interaction(pop_size, reasoning)), method = "loess", fill = 'orange') +
-  facet_wrap(~pop_size, scales = "free") +
+  #facet_wrap(~pop_size, scales = "free") +
+  scale_y_log10() +
+  labs(y= "Area Size", x = "Time (s)") +
   theme_bw() +
   theme(axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14),
         axis.text.x = element_text(size = 12),
         axis.text.y = element_text(size = 12))
+
+### Data analysis ####
+average_area_size <- area_longdata %>%
+  group_by(time, pop_size, reasoning) %>%
+  mutate(reasoning = factor(reasoning, levels = c("d0", "d1", "d2", "d3"))) %>% 
+  summarize(mean_area_size = mean(area_size, na.rm = TRUE),
+            median_area_size = median(area_size, na.rm = TRUE),
+            sd_area_size = sd(area_size, na.rm = TRUE),
+            .groups = "drop")
+ggplot(average_area_size, aes(x = time, y = mean_area_size, color = pop_size)) +
+  geom_line(linewidth = 1) +
+  facet_wrap(~reasoning) +
+  theme_bw()
+
+# Run a two-way ANOVA, pairwise comparison and plot emmeans
+anova_area <- aov(mean_area_size ~ reasoning * pop_size, data = average_area_size)
+summary(anova_area)
+
+emmeans(anova_area, ~ reasoning | pop_size)
+pairwise_area_df <- as.data.frame(emmeans(anova_area, ~ reasoning | pop_size))
+
+emmeans_area <- ggplot(pairwise_area_df, aes(x = reasoning, y = emmean, color = pop_size, group = pop_size)) +
+  geom_line(linewidth = 1) + geom_point(size = 2) +
+  labs(y= "EMMean Area", x = "Depth of Reasoning", color = "Population Size", fill = "Population Size") +
+  geom_ribbon(aes(ymin = lower.CL, ymax = upper.CL, fill = pop_size), alpha = 0.2, color = NA) + # Shaded CI
+  theme_bw()
+
+ggsave(file.path(file.path(directory_path, "Figures"), "emmeans.pdf"), plot = emmeans, width = 8, height = 6, units = "cm")
