1aad34d4dd11a7b9158ed0d899ebf55fe9cd07dc
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-02-06 14:09:37 +0100

    Animating area histograms successfully

--- animations.py
+++ animations.py
@@ -2,6 +2,7 @@
 # Dec 16, 2024
 # pminasandra.github.io
 
+import random
 from os.path import join as joinpath
 import os
 
@@ -66,14 +67,18 @@ def animate_area_hists(datasets, tmin=0, tmax=None, delay=None):
     if tmax is None:
         tmax = len(datasets[0])
     fig, ax = plt.subplots(1, len(datasets), sharey=True, figsize=(4.0, 4.0), dpi=300)
+    for i, a in enumerate(ax):
+        a.set_xscale('log')
+        a.set_xlabel('Area of Voronoi polygons')
+        a.set_title(f'$d_{i}$')
 
+    bins = np.logspace(np.log10(1e-8), np.log10(1),  50)
     hists = []
     j = 0
     for data in datasets:
-        print(len(np.log(data[0])))
-        hists.append(ax[j].hist(np.log(data[0])))
-        ax[j].set_xlim((-20, 0))
-        ax[j].set_ylim((0, 50))
+        hists.append(ax[j].hist(data[0], bins=bins))
+        ax[j].set_xlim((1e-8, 1))
+        ax[j].set_ylim((0, 350))
         j += 1
 
     if delay is None:
@@ -87,9 +92,13 @@ def animate_area_hists(datasets, tmin=0, tmax=None, delay=None):
 
         for hist_dat, a in zip(hists, ax):
             a.cla()
-            a.hist(np.log(hist_dat))
-            a.set_xlim((-20, 0))
-            a.set_ylim((0, 50))
+            a.hist(hist_dat, bins=bins)
+            a.set_xlim((1e-8, 1))
+            a.set_ylim((0, 350))
+        for i, a in enumerate(ax):
+            a.set_xscale('log')
+            a.set_xlabel('Area of Voronoi polygons')
+            a.set_title(f'$d_{i}$')
 
         return ax,
 
@@ -120,16 +129,16 @@ def _extract_areas(dataset):
 def _joinlists(lol1, lol2):
     lol_res = []
     for x, y in zip(lol1, lol2):
-        lol_res.append(x+y)
+        lol_res.append(list(x)+list(y))
 
     return lol_res
 
 if __name__ == "__main__":
     area_datasets = [[], []]
-    for j in [10, 270, 34, 399, 30, 76]:
+    for j in random.sample(range(500), 50):
         data = []
         for i in [0, 1]:
-            tgt_file = measurements._files_for(75, i)
+            tgt_file = measurements._files_for(50, i)
             tgt_file = list(tgt_file)[j]
             tgt_file = measurements._read_data(tgt_file)
             data.append(tgt_file)
@@ -138,16 +147,17 @@ if __name__ == "__main__":
         for dataset in data:
             areas_data.append(_extract_areas(dataset))
 
-        print(len(area_datasets[0]))
-        if area_datasets[0] == []:
+
+        if len(area_datasets[0]) == 0:
             area_datasets[0] = areas_data[0]
             area_datasets[1] = areas_data[1]
         else:
+#            print(len(area_datasets[0][0]))
 # this joining is fully improper
 # plan this properly
             area_datasets[0] = _joinlists(area_datasets[0], areas_data[0])
             area_datasets[1] = _joinlists(area_datasets[1], areas_data[1])
 
     print("Initiating animations")
-    anim = animate_area_hists(area_datasets, tmax=200, delay=0.5)
-    save_animation(anim, f"hist_75.gif")
+    anim = animate_area_hists(area_datasets, delay=0.5)
+    save_animation(anim, f"hist_50.gif")
