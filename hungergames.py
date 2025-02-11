7a9e7783908f8ca1fa4f5d7e5b0923e63718fb0a
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-02-11 16:30:57 +0100

    main.py adapted for hungergames

---
+++ hungergames.py
@@ -0,0 +1,51 @@
+# Pranav Minasandra
+# pminasandra.github.io
+# 11 Feb 2025
+
+from os.path import join as joinpath
+import uuid
+
+import numpy as np
+
+import config
+import selfishherd
+import utilities
+
+def hungergame(init_locs, num_smart):
+    """
+    Sets up and runs an individual contest, starting a smart selfish herd
+    of n individuals, of which the first num_smart are d_1 and the rest
+    are d_0.
+
+    Args:
+        init_locs (n×2 array-like): initial locations of agents
+        num_smart (int): how many d1 individuals 
+    """
+
+    num_inds = init_locs.shape[0]
+    depths = np.zeros(num_inds).astype(int)
+    depths[:num_smart] += 1
+
+    herd = selfishherd.SelfishHerd(num_inds, depths, init_locs)
+    uname = str(uuid.uuid4())
+    fname = joinpath(config.DATA, "HungerGames",
+                f"{num_inds}-n{num_smart}-{uname}.pkl")
+
+    return herd, fname
+
+
+def hungergames(popsize, num_smart, num_instances):
+    """
+    *GENERATOR*
+    Wrapper around hungergame(...)
+    Args:
+        popsize (int): population size
+        num_smart (int): number of d_1 inds
+        num_instances (int): how many simulations are needed
+    """
+
+    for i in range(num_instances):
+        init_locs = np.random.uniform(size=(popsize, 2))
+        herd, fname = hungergame(init_locs, num_smart)
+
+        yield herd, fname
