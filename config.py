bce05e868c31730669e4e3d620c54150671256f1
Pranav Minasandra (Dell) <pminasandra@ab.mpg.de>
2025-02-12 18:31:07 +0100

    Prettier organisation of flow config

--- config.py
+++ config.py
@@ -13,14 +13,20 @@ FIGURES = os.path.join(PROJECTROOT, "Figures")
 
 formats=['png', 'pdf', 'svg']
 
+# Program flow
+RUN_SIMS = False
+CONDUCT_HUNGERGAMES = True
+ANALYSE_DATA = False
+ANALYSE_HUNGERGAMES = True
+
+
 # Gradient descent config
 GRAD_DESC_DX = 0.005
 GRAD_DESC_DY = 0.005
 GRAD_DESC_MAX_STEP_SIZE = 0.05
 GRAD_DESC_MULTPL_FACTOR = 0.1
 
-# Program flow
-RUN_SIMS = False
+# Strucuring 
 POP_S_DOR = {
 10: [0, 1, 2, 3],
 25: [0, 1, 2, 3],
@@ -34,14 +40,14 @@ NUM_REPEATS = 500
 TMAX = 500
 
 # Program flow for hungergames
-CONDUCT_HUNGERGAMES = True
 POP_S_SMART_GUYS_HG = {
     25: [5, 10, 15, 20],
     87: [5, 25, 45, 65]
 } # these are how many d1 individuals to have in each round
 
+HUNGERGAMES_TIME_LIMS = (150, 250)
+
 # Data analysis
-ANALYSE_DATA = False
 ANALYSE_POP_SIZES = [10, 25, 35, 50, 75, 87, 100]
 ANALYSE_DEPTHS = [0, 1, 2, 3]
 
