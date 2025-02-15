926acc71c227255bdf8b16916c3d8ce16679bc7e
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-02-15 13:24:56 +0100

    Extraction of relevant data has proceeded further

--- hungergames.py
+++ hungergames.py
@@ -4,6 +4,7 @@
 
 import glob
 from os.path import join as joinpath
+import pickle
 import uuid
 
 import numpy as np
@@ -12,6 +13,7 @@ import config
 import measurements
 import selfishherd
 import utilities
+import voronoi
 
 def hungergame(init_locs, num_smart):
     """
@@ -71,7 +73,7 @@ def _read_hungergames_data(filename):
 def extract_groupsizes(dataset, num_smart):
     """
     For a given dataset containing d_0 and d_1 individuals,  returns TGS
-    fo d_0 and d_1 individuals separately.
+    for d_0 and d_1 individuals separately.
 
     Args:
         dataset (array-like, n×2×t): location data across time.
@@ -93,7 +95,7 @@ def extract_groupsizes(dataset, num_smart):
         group_sizes_per_ind = np.zeros(group_ids.shape)
         group_labels, group_sizes = np.unique(group_ids, return_counts=True)
         
-        for group in group_ids.unique():
+        for group in np.unique(group_ids):
             index = group_labels == group #mask
             assert sum(index) == 1 # can only be one with this label
             size = group_sizes[index][0]
@@ -107,4 +109,63 @@ def extract_groupsizes(dataset, num_smart):
 
     values_across_time = np.array(values_across_time)
 
-    return values_across_time.mean(axis=0)
+    results =  values_across_time.mean(axis=0)
+    return results[0], results[1]
+
+
+def extract_areas(dataset, num_smart):
+    """
+    For a given dataset containing d_0 and d_1 individuals,  returns mean Voronoi areas
+    for d_0 and d_1 individuals separately.
+
+    Args:
+        dataset (array-like, n×2×t): location data across time.
+        num_smart (int): starting from index 0, how many d_1 individuals.
+
+    Returns:
+        np.array: (area_d_0, area_d_1)
+    """
+    dataset = dataset.copy()[:,:,config.HUNGERGAMES_TIME_LIMS[0]:
+                            config.HUNGERGAMES_TIME_LIMS[1]]
+
+
+    ttotal = dataset.shape[2]
+    values_across_time = []
+    for t in range(0, ttotal, 20):
+        data_sub = dataset[:,:,t]
+        vor = voronoi.get_bounded_voronoi(data_sub)
+        areas = voronoi.get_areas(data_sub, vor)
+
+        area_d0 = np.log(areas[num_smart:]).mean()
+        area_d1 = np.log(areas[:num_smart]).mean()
+        values_across_time.append([area_d0, area_d1])
+
+    values_across_time = np.array(values_across_time)
+
+    results =  values_across_time.mean(axis=0)
+    return results[0], results[1]
+
+
+def extract_all_data():
+    """
+    Runs above analyses on all simulated hungergames data.
+    """
+
+    for popsize in config.POP_S_SMART_GUYS_HG:
+        for num_smart in config.POP_S_SMART_GUYS_HG[popsize]:
+            files = _hungergames_files_for(popsize, num_smart)
+            gsizes, areas = [], []
+            for file_ in files:
+                data = _read_hungergames_data(file_)
+                group_s_d0, group_s_d1 = extract_groupsizes(data, num_smart)
+                area_d0, area_d1 = extract_areas(data, num_smart)
+
+                gsizes.append([group_s_d0, group_s_d1])
+                areas.append([area_d0, area_d1])
+            gsizes = np.array(gsizes)
+            areas = np.array(areas)
+
+            print(popsize, num_smart, "\n", areas[:,0]-areas[:,1])
+        print()
+
+extract_all_data()
